ff1491603378dbe75588829d06710341e4bdf24a
YangZiver <3394689581@qq.com>
2025-08-24 14:26:56 +0800

    second edition

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+# 看这里
+这是我在Github上传的第一个代码
+This is my first code committed to Github!
--- test1.cpp
+++ test1.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int main()
 {
     cout << "hello,world!" << endl;
+    cout << "This is my first code committed to Github!" << endl;
     return 0;
 }
